e80ef5bfd6c7f714ccf7818c85f1310bee8c4741
Desale94 <121386567+Desale94@users.noreply.github.com>
2023-02-09 22:43:51 +0530

    Print Max Number at 3rd Position

--- MaxPosition.java
+++ MaxPosition.java
@@ -1,18 +1,9 @@
 package com.bridgelabz.Maximum;
 
-import java.util.Scanner;
-
 public class MaxPosition {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        System.out.println("Enter size of array");
-        int n = s.nextInt();
-        int arr[] = new int[n];
-        System.out.println("Enter array element");
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = s.nextInt();
-        }
-
+        int arr[] ={21,3,12,22};
+      
         System.out.println("Array elements ");
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
