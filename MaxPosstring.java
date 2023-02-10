78a8fbca88238a7fb8ce09fd8277db7886e7fbfc
Desale1 <dkdesale08@gmail.com>
2023-02-10 13:48:11 +0530

    Print maximum string at 1st position

---
+++ MaxPosstring.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.Maximum;
+
+public class MaxPosstring {
+    public static void main(String[] args) {
+        String arr[]={"peach","apple","banana"};
+
+        System.out.println("Array elements ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println(" ");
+        String max = arr[0];
+        int index=0, i;
+        for (i=0;i<arr.length;i++) {
+            if (arr[i].compareTo( max)>0) {
+                max = arr[i];
+                index=i;
+            }
+        }
+        System.out.println("Maximum String: " +max);
+        System.out.println("Position of max string is: " +index);
+
+    }
+}
